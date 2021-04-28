f82c0d430c39553bb1f13bcb0ec20e8efe132ffd
Mariana10-04 <71359480+Mariana10-04@users.noreply.github.com>
2021-04-27 23:07:36 -0300

    Resources

--- prova27.04/src/main/java/com/marianaa/prova2704/Resources.java
+++ prova27.04/src/main/java/com/marianaa/prova2704/Resources.java
@@ -1,5 +1,41 @@
 package com.marianaa.prova2704;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response;
+
+import com.marianaa.prova2704.controller.categoriaController;
+import com.marianaa.prova2704.controller.filmeController;
+import com.marianaa.prova2704.model.Categoria;
+
+@Path("/")
 public class Resources {
+
+    filmeController filmeController = new filmeController();
+    categoriaController categoriaController = new categoriaController();
+
+    @GET
+    @Path("/filmes")
+    @Produces("application/json")
+    public Response listFilmes(){
+        return filmeController.listFilme();
+    }
+
+    @GET
+    @Path("/filmes")
+    @Produces("application/json")
+    @Consumes("application/json")
+    public Response listCategoria(){
+        return categoriaController.listCategoria();
+    }
     
+    @GET
+    @Path("/filmes")
+    @Produces("application/json")
+    @Consumes("application/json")
+    public Response addCategoria (Categoria categoria){
+        return categoriaController.addCategoria(categoria);
+    }
 }
